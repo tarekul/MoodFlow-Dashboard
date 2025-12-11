5bed1a0dfd2b8d10839bc0942f00be5f7dd3df50
tarekul <rahin6810@gmail.com>
2025-12-11 03:04:15 -0500

    enable pwa for app download

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,47 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "MoodFlow",
+        short_name: "MoodFlow",
+        description: "Discover your unique productivity drivers",
+        theme_color: "#6366f1",
+        background_color: "#eff6ff",
+        display: "standalone",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
