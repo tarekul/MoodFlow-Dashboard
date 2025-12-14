8a73106bda2af515be8329b51f04054c7f23dbef
tarekul <rahin6810@gmail.com>
2025-12-13 19:46:04 -0500

    move google client id to env

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const GOOGLE_CLIENT_ID =
-  "899157555176-dj3rrj6adsv2l1vr1oskn4bklbquho6j.apps.googleusercontent.com";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>
