b4db43c7134a0698645c6105a557296ac8eb5b3b
tarekul <rahin6810@gmail.com>
2025-12-18 01:37:00 -0500

    center progrss bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,6 +1,6 @@
 const ProgressBar = ({ currentStep }) => {
   return (
-    <div className="w-full max-w-xl mt-16 mb-4">
+    <div className="w-full max-w-xl mt-4 mb-4 mx-auto">
       <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden">
         <div
           className="h-2 bg-indigo-600 rounded-full transition-all duration-300"
